5c1062d56272e11ae095954c2ca50909ff62240a
impony <impony@vip.qq.com>
2013-05-22 18:14:28 +0800

    fix bug

--- includes/in.js
+++ includes/in.js
@@ -30,13 +30,17 @@ window.addEventListener("DOMContentLoaded", function () {
 	//如果接收到 background script 的消息
 	opera.extension.addEventListener("message", function (event) {
 		var msg = event.data;
+		var custom_style;
 		//如果传过来的消息的 name 为某特定值
 		if(msg.name === "impony") {
 			reRender(msg.value[0], G_G.styleid);
 			if(widget.preferences.pagewidth === "narrow") {
 				reRender(msg.value[1], G_G.styleid_narrow);
 			}
-			document.getElementById("custom_style").innerHTML = document.getElementById("custom_style").innerHTML.match(/@import.+/gi)[0].replace(/diy\d+/gi, widget.preferences.pagecolor);
+			try {
+				custom_style = document.getElementById("custom_style");
+				custom_style.innerHTML = custom_style.innerHTML.match(/@import.+/gi)[0].replace(/diy\d+/gi, widget.preferences.pagecolor);
+			} catch(e) {}
 			if(window.location.host.indexOf("photo.weibo.com") != -1) {
 				document.body.style.backgroundColor = "#FFF";
 			}
